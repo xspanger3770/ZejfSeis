ea22d48e74e630f1fd3f86bcf45b56cdfacea0eb
Jakub Španger <xspanger@fi.muni.cz>
2022-11-11 16:46:34 +0100

    release 4.3.2

--- src/main/java/com/morce/zejfseis4/main/ZejfSeis4.java
+++ src/main/java/com/morce/zejfseis4/main/ZejfSeis4.java
@@ -14,7 +14,7 @@ import com.morce.zejfseis4.ui.ZejfSeisFrame;
 public class ZejfSeis4 {
 	
 	public static final File MAIN_FOLDER = new File("./ZejfSeis4/");
-	public static final String VERSION = "4.3.1";
+	public static final String VERSION = "4.3.2";
 	public static final int COMPATIBILITY_VERSION = 4;
 	
 	private static ZejfSeisFrame frame;
