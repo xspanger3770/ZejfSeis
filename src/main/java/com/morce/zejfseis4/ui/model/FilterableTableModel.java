2ec1d8aa0bf945239692069fc4b1f721bf13f438
Jakub Španger <xspanger@fi.muni.cz>
2022-12-16 16:45:05 +0100

    Fix: mutex in filterableTableModel

--- src/main/java/com/morce/zejfseis4/ui/model/FilterableTableModel.java
+++ src/main/java/com/morce/zejfseis4/ui/model/FilterableTableModel.java
@@ -7,66 +7,73 @@ import javax.swing.table.AbstractTableModel;
 
 public abstract class FilterableTableModel<E> extends AbstractTableModel {
 
-    private static final long serialVersionUID = 1727941556193013022L;
+	private static final long serialVersionUID = 1727941556193013022L;
 	private final List<E> data;
-    private final List<E> filteredData;
+	private final List<E> filteredData;
 
-    public FilterableTableModel(List<E> data){
-        this.data = data;
-        this.filteredData = new ArrayList<E>(data);
-        applyFilter();
-    }
+	public FilterableTableModel(List<E> data) {
+		this.data = data;
+		this.filteredData = new ArrayList<E>(data);
+		applyFilter();
+	}
 
-    public final void applyFilter(){
-        this.filteredData.clear();
-        this.filteredData.addAll(this.data.stream().filter(this::accept).toList());
-        super.fireTableDataChanged();
-    }
+	public final void applyFilter() {
+		synchronized (this) {
+			this.filteredData.clear();
+			this.filteredData.addAll(this.data.stream().filter(this::accept).toList());
+		}
+		super.fireTableDataChanged();
+	}
 
-    public boolean accept(E entity){
-        return true;
-    }
+	public boolean accept(E entity) {
+		return true;
+	}
 
-    public void dataUpdated() {
+	public void dataUpdated() {
 
-    }
+	}
 
-    @Override
-    public int getRowCount() {
-        return filteredData.size();
-    }
+	@Override
+	public synchronized int getRowCount() {
+		return filteredData.size();
+	}
 
-    public void updateRow(E entity) {
-        int rowIndex = filteredData.indexOf(entity);
-        fireTableRowsUpdated(rowIndex, rowIndex);
-        dataUpdated();
-    }
+	public void updateRow(E entity) {
+		int rowIndex;
+		synchronized (this) {
+			rowIndex = filteredData.indexOf(entity);
+		}
+		fireTableRowsUpdated(rowIndex, rowIndex);
+		dataUpdated();
+	}
 
-    public void deleteRow(int rowIndex) {
-        E entity = filteredData.get(rowIndex);
-        filteredData.remove(entity);
-        data.remove(entity);
-        fireTableRowsDeleted(rowIndex, rowIndex);
-        dataUpdated();
-    }
+	public synchronized void deleteRow(int rowIndex) {
+		E entity;
+		synchronized (this) {
+			entity = filteredData.get(rowIndex);
+			filteredData.remove(entity);
+		}
+		data.remove(entity);
+		fireTableRowsDeleted(rowIndex, rowIndex);
+		dataUpdated();
+	}
 
-    public void addRow(E entity) {
-        int newRowIndex = filteredData.size();
-        data.add(entity);
-        filteredData.add(entity);
-        fireTableRowsInserted(newRowIndex, newRowIndex);
-        dataUpdated();
-    }
+	public void addRow(E entity) {
+		int newRowIndex;
+		synchronized (this) {
+			newRowIndex = filteredData.size();
+			filteredData.add(entity);
+		}
+		data.add(entity);
+		fireTableRowsInserted(newRowIndex, newRowIndex);
+		dataUpdated();
+	}
 
-    public E getEntity(int rowIndex) {
-        return filteredData.get(rowIndex);
-    }
+	public synchronized E getEntity(int rowIndex) {
+		return filteredData.get(rowIndex);
+	}
 
-    public List<E> getFilteredData() {
-        return filteredData;
-    }
-
-    public List<E> getData() {
-        return data;
-    }
+	public List<E> getData() {
+		return data;
+	}
 }
