8d06c1eb69481b70c5bd98d3e68b5656892b7d6c
Jakub Španger <jakubspanger1025@gmail.com>
2025-06-16 18:21:03 +0200

    version auto generate

--- src/main/java/zejfseis4/main/ZejfSeis4.java
+++ src/main/java/zejfseis4/main/ZejfSeis4.java
@@ -20,107 +20,108 @@ import zejfseis4.ui.ZejfSeisFrame;
 
 public class ZejfSeis4 {
 
-	public static final File MAIN_FOLDER = new File("./ZejfSeis4/");
-	public static final String VERSION = "4.7.0";
-	public static final int COMPATIBILITY_VERSION = 4;
+    public static final File MAIN_FOLDER = new File("./ZejfSeis4/");
+    public static final String VERSION = zejfseis4.main.Version.VERSION;
+    public static final int COMPATIBILITY_VERSION = 4;
 
-	public static boolean DEBUG = true; // always debug :)
+    public static boolean DEBUG = true; // always debug :)
 
-	private static ZejfSeisFrame frame;
-	private static DataManager dataManager;
-	private static ZejfClient client;
-	private static EventManager eventManager;
-	private static ApplicationErrorHandler errorHandler;
-	private static ZejfSerialReader serialReader;
+    private static ZejfSeisFrame frame;
+    private static DataManager dataManager;
+    private static ZejfClient client;
+    private static EventManager eventManager;
+    private static ApplicationErrorHandler errorHandler;
+    private static ZejfSerialReader serialReader;
 
-	public static void init() {
-		frame = new ZejfSeisFrame();
-		errorHandler = new ApplicationErrorHandler(frame);
-		Thread.setDefaultUncaughtExceptionHandler(errorHandler);
-		if (!MAIN_FOLDER.exists()) {
-			if (!MAIN_FOLDER.mkdirs()) {
-				System.err.println("Failed to create main dir");
-				return;
-			}
-		}
+    public static void init() {
+        frame = new ZejfSeisFrame();
+        errorHandler = new ApplicationErrorHandler(frame);
+        Thread.setDefaultUncaughtExceptionHandler(errorHandler);
+        if (!MAIN_FOLDER.exists()) {
+            if (!MAIN_FOLDER.mkdirs()) {
+                System.err.println("Failed to create main dir");
+                return;
+            }
+        }
 
-		try {
-			Settings.loadProperties();
-			Scales.loadScales();
-		} catch (FatalIOException e3) {
-			handleException(e3);
-		}
+        try {
+            Settings.loadProperties();
+            Scales.loadScales();
+        } catch (FatalIOException e3) {
+            handleException(e3);
+        }
 
-		client = new ZejfClient();
-		serialReader = new ZejfSerialReader();
-		dataManager = new DataManager();
-		try {
-			dataManager.loadFromInfo();
-		} catch (FatalIOException e2) {
-			handleException(e2);
-		}
+        client = new ZejfClient();
+        serialReader = new ZejfSerialReader();
+        dataManager = new DataManager();
+        try {
+            dataManager.loadFromInfo();
+        } catch (FatalIOException e2) {
+            handleException(e2);
+        }
 
-		eventManager = new EventManager();
+        eventManager = new EventManager();
 
-		SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
 
-			@Override
-			public void run() {
-				frame.createFrame();
-				frame.setVisible(true);
+            @Override
+            public void run() {
+                frame.createFrame();
+                frame.setVisible(true);
 
-				frame.addWindowListener(new WindowAdapter() {
-					@Override
-					public void windowClosing(WindowEvent e) {
-						windowClosed(e);
-					}
-					@Override
-					public void windowClosed(WindowEvent e) {
-						try {
-							dataManager.exit();
-						} catch (FatalIOException e1) {
-							handleException(e1);
-						}
-						try {
-							eventManager.saveAll();
-						} catch (FatalIOException e1) {
-							handleException(e1);
-						}
-					}
-				});
-				
-				getFrame().setStatus("Idle");
-			}
-		});
+                frame.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        windowClosed(e);
+                    }
 
-	}
+                    @Override
+                    public void windowClosed(WindowEvent e) {
+                        try {
+                            dataManager.exit();
+                        } catch (FatalIOException e1) {
+                            handleException(e1);
+                        }
+                        try {
+                            eventManager.saveAll();
+                        } catch (FatalIOException e1) {
+                            handleException(e1);
+                        }
+                    }
+                });
 
-	public static ZejfSeisFrame getFrame() {
-		return frame;
-	}
+                getFrame().setStatus("Idle");
+            }
+        });
 
-	public static DataManager getDataManager() {
-		return dataManager;
-	}
+    }
 
-	public static ZejfClient getClient() {
-		return client;
-	}
-	
-	public static ZejfSerialReader getSerialReader() {
-		return serialReader;
-	}
+    public static ZejfSeisFrame getFrame() {
+        return frame;
+    }
 
-	public static EventManager getEventManager() {
-		return eventManager;
-	}
+    public static DataManager getDataManager() {
+        return dataManager;
+    }
 
-	public static void handleException(Throwable e) {
-		if (!(e instanceof RuntimeApplicationException)) {
-			Logger.error("Caught exception : {}", e.getMessage());
-			Logger.error(e);
-		}
-		errorHandler.handleException(e);
-	}
+    public static ZejfClient getClient() {
+        return client;
+    }
+
+    public static ZejfSerialReader getSerialReader() {
+        return serialReader;
+    }
+
+    public static EventManager getEventManager() {
+        return eventManager;
+    }
+
+    public static void handleException(Throwable e) {
+        if (!(e instanceof RuntimeApplicationException)) {
+            Logger.error("Caught exception : {}", e.getMessage());
+            Logger.error(e);
+        }
+        errorHandler.handleException(e);
+    }
 
 }
---
+++ src/main/templates/Version.java
@@ -0,0 +1,5 @@
+package zejfseis4.main;
+
+public final class Version {
+    public static final String VERSION = "${project.version}";
+}
