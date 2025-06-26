9cc96b491fdba424caf67b60e5d7ba79a3677caa
Jakub Španger <jakubspanger1025@gmail.com>
2025-06-26 16:27:25 +0200

    add running Exponential Moving Average offset calibration to ads1263 code

--- arduino/ADS1263_Seismo/ADS1263_Seismo.ino
+++ arduino/ADS1263_Seismo/ADS1263_Seismo.ino
@@ -13,14 +13,15 @@
 
 // ADS1263 OVERSAMPLING
 
-#define ADS_SAMPLE_RATE ADS126X_RATE_1200
-const double ads_sample_rate = 1200.0;
-const double sampling_time = 1.0 / ads_sample_rate;
+#define ADS_output_sample_rate ADS126X_RATE_1200
+const double ads_output_sample_rate = 1200.0;
+const double input_sampling_time = 1.0 / ads_output_sample_rate;
 
 // OFFSET CALIBRATION
 
 const int calibration_seconds = 60;
 const int initial_ignore_seconds = 12;
+const double continuous_offset_fade_time_seconds = 600.0;
 
 // FILTER
 
@@ -28,27 +29,29 @@ float cutoff_freq = 20.0;
 IIR::ORDER order = IIR::ORDER::OD4;
 
 // INTERNAL (DO NOT CHANGE AFTER THIS LINE)
-Filter filter(cutoff_freq, sampling_time, order);
+Filter filter(cutoff_freq, input_sampling_time, order);
 ADS126X adc;
-const int SAMPLE_RATES[5] = { 20, 40, 60, 100, 200 };
+const int output_sample_rateS[5] = { 20, 40, 60, 100, 200 };
 
-int sample_rate;
-long sample_time_micros;
+int output_sample_rate;
+long output_sample_time_micros;
 
 bool calibrating;
-int32_t offset;
+
+double offset = 0.0;
+double offset_alpha = input_sampling_time / continuous_offset_fade_time_seconds;
 
 unsigned long last_time;
 unsigned long current_time;
 
-int64_t sum;
+double sum;
 int32_t count;
 int32_t calibration_count;
 
 byte log_num;
 int shift;
 
-void get_sample_rate()
+void get_output_sample_rate()
 {
     int index = -1;
     Serial.println("Please select sample rate");
@@ -64,14 +67,14 @@ void get_sample_rate()
     }
 
     shift = 0;
-    sample_rate = SAMPLE_RATES[index];
-    sample_time_micros = 1000000 / sample_rate;
+    output_sample_rate = output_sample_rateS[index];
+    output_sample_time_micros = 1000000 / output_sample_rate;
 
-    cutoff_freq = sample_rate / 2.0;
+    cutoff_freq = output_sample_rate / 2.0;
     filter.setCutoffFreqHZ(cutoff_freq);
 
     Serial.print("Selected ");
-    Serial.print(sample_rate);
+    Serial.print(output_sample_rate);
     Serial.println(" sps");
 
     last_time = 0;
@@ -91,11 +94,11 @@ void setup()
 
     pinMode(PIN_DRDY, INPUT);
 
-    get_sample_rate();
+    get_output_sample_rate();
 
     adc.begin(PIN_CS);
     delay(10);
-    adc.setRate(ADS_SAMPLE_RATE);
+    adc.setRate(ADS_output_sample_rate);
     delay(10);
     adc.startADC1();
     delay(10);
@@ -114,30 +117,35 @@ void loop()
     if (rdy) {
         int32_t current_val = adc.readADC1(POS_PIN, NEG_PIN);
         if (calibrating) {
-            if( calibration_count >= initial_ignore_seconds * ads_sample_rate) {
+            if( calibration_count >= initial_ignore_seconds * ads_output_sample_rate) {
                 sum += current_val;
                 count++;
             }
 
             calibration_count++;
 
-            if (count >= calibration_seconds * ads_sample_rate) {
+            if (count >= calibration_seconds * ads_output_sample_rate) {
                 calibrating = false;
-                offset = lround(sum / count);
-                sum = 0;
+                offset = double(sum) / double(count);
+                sum = 0.0;
                 count = 0;
                 Serial.print("Offset calibration finish, offset = ");
                 Serial.println(offset);
             }
         } else {
-            sum += filter.filterIn(current_val - offset);
-            count++;
+            offset += offset_alpha * (current_val - offset);
+
+            int32_t corrected = current_val - lround(offset);
+            float y = filter.filterIn(corrected);
+
+            sum += y;
+            count += 1;
         }
     }
 
     current_time = micros();
 
-    if (!calibrating && abs(current_time - last_time) >= sample_time_micros + shift) {
+    if (!calibrating && (current_time - last_time) >= (unsigned long)output_sample_time_micros + shift) {
         int32_t value = lround(sum / count);
         sum = 0;
         count = 0;
@@ -160,7 +168,7 @@ void loop()
         } else if (ch == '+' && shift != INT_MAX) {
             shift++;
         } else if (ch == 'r') {
-            get_sample_rate();
+            get_output_sample_rate();
         } else if (ch == 'c') {
             adc.calibrateSysOffsetADC1(POS_PIN, NEG_PIN);
         } else if (ch == 's') {
